9ebf855030cc75aab40aa650c4c8941a7e32db25
plantapi662-lab <plantapi662@gmail.com>
2026-02-10 20:53:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
-// server.js
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/script', (req, res) => {
-    // Code Lua que tu veux renvoyer à Roblox
-    const luaScript = `
-        -- Exemple de script Lua exécuté dans Roblox
-        print("Bonjour depuis le serveur !")
-        -- Charger et exécuter le script depuis GitHub
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/plantapi662-lab/APSPAMMERSAB/refs/heads/main/W"))()
-    `;
-    
-    res.type('text/plain');  // Indiquer que la réponse est du texte
-    res.send(luaScript);     // Renvoyer le code Lua
+app.get("/", (req, res) => {
+  const luaScript = `loadstring(game:HttpGet("https://raw.githubusercontent.com/plantapi662-lab/APSPAMMERSAB/refs/heads/main/W"))()`;
+  res.type("text/plain");
+  res.send(luaScript);
 });
 
-app.listen(port, () => {
-    console.log(`Serveur démarré sur le port ${port}`);
-});
+app.listen(port, () => console.log("Server running on port " + port));
